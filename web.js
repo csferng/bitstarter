651efad2db40da60464e8eeb5e0e746286899bf0
Chun-Sung Ferng <csferng@acm.(none)>
2013-07-07 16:28:01 +0800

    Fix bug in web.js

--- web.js
+++ web.js
@@ -4,10 +4,9 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var content = fs.readFile('index.html');
+  var content = fs.readFileSync('index.html');
   var buffer = new Buffer(content, "utf-8");
   response.send(buffer.toString());
-//  response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
